8b67623a7fdd9889ccfcf1e32042584db80eb973
Kacper Biaduń <kacperbiadun.12@gmail.com>
2024-10-23 18:39:27 +0200

    Initial Commit

---
+++ ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by Kacper Biaduń on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
